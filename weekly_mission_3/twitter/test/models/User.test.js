05c14831b7922f3e7f01a7e65f0f6686454de0ef
RaulG <raul.animations@gmail.com>
2022-04-30 21:58:28 -0500

    Requerimiento 4: Se agregan los setters al modelo User y se realiza ajuste en las pruebas

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -3,11 +3,10 @@ const User = require('./../../app/models/User')
 describe("Unit test for User object", () => {
     test('Create an User Object', () => {
         const user = new User(1, "raulggg", "Raul", "Bio")
+        user.setUsername = "PedroFuentes"
+        expect(user.username).toBe("PedroFuentes")
 
-        expect(user.id).toBe(1)
-        expect(user.getUsername).toBe("raulggg")
-        expect(user.getBio).toBe("Bio")
-        expect(user.getDateCreated).not.toBeUndefined()
-        expect(user.getLastUpdated).not.toBeUndefined()
+        user.setBio = "New Bio"
+        expect(user.bio).toBe("New Bio")
     })
 })
