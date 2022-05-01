7abcea625bd9ac99111297eb8cec7917504cdbfe
RaulG <raul.animations@gmail.com>
2022-04-30 21:33:11 -0500

    Se modifica la prueba del modelo User para no aceptar undefined en dos de sus propiedades

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -2,13 +2,13 @@ const User = require('./../../app/models/User')
 
 describe("Unit test for User object", () => {
     test('Create an User Object', () => {
-        const user = new User(1, "raulggg", "Raul", "Bio", "10/05/2022", "10/05/2022")
+        const user = new User(1, "raulggg", "Raul", "Bio")
 
         expect(user.id).toBe(1)
         expect(user.username).toBe("raulggg")
         expect(user.name).toBe("Raul")
         expect(user.bio).toBe("Bio")
-        expect(user.dataCreated).toBe("10/05/2022")
-        expect(user.lastUpdated).toBe("10/05/2022")
+        expect(user.dataCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
