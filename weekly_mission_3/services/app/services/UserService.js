99ed8764adbcc49a1b81d691b69a809bcfd51df0
RaulG <raul.animations@gmail.com>
2022-05-01 12:55:28 -0500

    Services: Requerimiento 2, se crea el metodo getInfo y se realiza la prueba correspondiente

--- weekly_mission_3/services/app/services/UserService.js
+++ weekly_mission_3/services/app/services/UserService.js
@@ -4,6 +4,14 @@ class UserService {
     static create(id, username, name) {
         return new User(id, username, name, "Bio")
     }
+
+    static getInfo(user) {
+        var data = [];
+        for(var key in user){
+            data.push(user[key]);
+        }
+        return data;
+    }
 }
 
 module.exports = UserService
