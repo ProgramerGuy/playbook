d1830cf1a8164b862c373fb55ff3bc06dc731bd2
RaulG <raul.animations@gmail.com>
2022-05-01 21:03:40 -0500

    Views: se crean las pruebas para el metodo createUser

---
+++ weekly_mission_3/services/test/views/UserView.test.js
@@ -0,0 +1,35 @@
+const UserView = require("./../../app/views/UserView")
+
+
+describe("Test for UserView", () => {
+    test("Return an error object when try to create a new user with an null payload", () => {
+        const payload = null
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/payload no existe/)
+    })
+
+    test("Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {usernam: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+
+    test("Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {usernam: "MarceloBRR"}
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+
+    test("Create a user by a given valid payload", () => {
+        const payload = {usernam: "MarceloBRR", id: 1, name: "Marcelo"}
+        const result = UserView.createUser(payload)
+
+        expect(result.name).toBe("Marcelo")
+        expect(result.username).toBe("MarceloBRR")
+        expect(result.id).toBe(1)
+    })
+
+})
